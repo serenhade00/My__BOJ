69a2af052c87d7c517beec1e81f262905837f4bc
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2022-08-29 22:00:29 +0900

    [Gold IV] Title: 네트워크 연결, Time: 36 ms, Memory: 6564 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+
+using namespace std;
+
+// 비용, 정점 번호
+vector<pair<int,int>> edge[1005];
+priority_queue<pair<int,int>,
+               vector<pair<int,int>>,
+               greater<pair<int,int>>> pq;
+bool chk[1005];
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int v, e, st, en, cost;
+    cin >> v >> e;
+    for(int i=0; i<e; i++)
+    {
+        cin >> st >> en >> cost;
+        edge[st].push_back({cost, en});
+        edge[en].push_back({cost, st});
+    }
+    
+    chk[1] = 1;
+    for(auto nxt : edge[1])
+        pq.push({nxt.X, nxt.Y});
+    
+    int cnt = 0, sum = 0;
+    while(cnt < v-1)
+    {
+        int cost, cur;
+        tie(cost, cur) = pq.top();
+        pq.pop();
+        
+        if(chk[cur])
+            continue;
+        chk[cur] = 1;
+        for(auto nxt : edge[cur])
+        {
+            if(!chk[nxt.Y])
+                pq.push({nxt.X, nxt.Y});
+        }
+        cnt++;
+        sum += cost;
+    }
+    cout << sum;
+}
