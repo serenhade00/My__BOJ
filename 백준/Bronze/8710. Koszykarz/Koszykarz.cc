6835887f9e517cdbaf0520d73f224727eed0507b
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2022-09-02 00:31:23 +0900

    [Bronze IV] Title: Koszykarz, Time: 344 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8710. Koszykarz/Koszykarz.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    int cnt = 0;
+    while(a < b)
+    {
+        a += c;
+        cnt++;
+    }
+    cout << cnt;
+}
