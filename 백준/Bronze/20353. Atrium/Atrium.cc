b96b9d6d2d59ff4360995f7165ea3a6928888791
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2022-09-20 00:45:51 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    long long n;
+    cin >> n;
+    
+    cout << fixed;
+    cout.precision(8);
+    
+    cout << sqrt(n)*4;
+}
