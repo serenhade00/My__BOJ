e0670fe48ff202fd55d5caad0af7719c1b450875
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2022-12-27 00:16:33 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        int num;
+        cin >> num;
+        cout << num << ' ' << num << '\n';
+    }
+}
