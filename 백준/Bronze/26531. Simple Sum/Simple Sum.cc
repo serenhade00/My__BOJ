ed00f568f96cbfecaae7a92b4d0a4bf7700d5096
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-01-10 02:30:41 +0900

    [Bronze IV] Title: Simple Sum, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b, c;
+    char d, e;
+    
+    cin >> a >> d >> b >> e >> c;
+    
+    if(a+b == c)
+        cout << "YES";
+    else
+        cout << "NO";
+}
