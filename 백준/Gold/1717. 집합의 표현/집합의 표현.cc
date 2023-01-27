c8b9a4d982aea5dbc4a3388b74d1ab50ebee7710
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-01-27 17:27:02 +0900

    [Gold IV] Title: 집합의 표현, Time: 40 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> p(1000001, -1);
+
+int get_find(int x)
+{
+    if(p[x] < 0)
+        return x;
+    return p[x] = get_find(p[x]);
+}
+
+int get_union(int u, int v)
+{
+    u = get_find(u), v = get_find(v);
+    if(u == v)
+        return 0;
+    if(p[u] == p[v])
+        p[u]--;
+    if(p[u] < p[v])
+        p[v] = u;
+    else
+        p[u] = v;
+    return 1;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    for(int i=0; i<m; i++)
+    {
+        int val, a, b;
+        cin >> val >> a >> b;
+        if(!val)
+            get_union(a, b);
+        else
+        {
+        	a = get_find(a), b = get_find(b);
+            if(a == b)
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+    }
+}
