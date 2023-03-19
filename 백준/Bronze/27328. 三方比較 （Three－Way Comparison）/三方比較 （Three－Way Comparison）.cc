05713c14697c03ce730f73a753246ca051ef005a
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-03-20 02:15:13 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+    cin >> a >> b;
+    if(a < b)
+        cout << -1;
+    else if(a == b)
+        cout << 0;
+    else
+        cout << 1;
+}
