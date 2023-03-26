ab8432c490c993507081f7bf6a3ba3afbee0b4ff
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-03-26 19:53:38 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    string s;
+    cin >> s;
+    if(s == "NLCS")
+        cout << "North London Collegiate School";
+    else if(s == "BHA")
+        cout << "Branksome Hall Asia";
+    else if(s == "KIS")
+        cout << "Korea International School";
+    else
+        cout << "St. Johnsbury Academy";
+}
