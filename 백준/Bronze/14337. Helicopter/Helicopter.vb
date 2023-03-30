d32df036b0e9f61aed6cc9b766beaefd8462d9c8
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-03-31 03:10:13 +0900

    [Bronze V] Title: Helicopter, Time: 60 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,12 @@
+Imports System
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("      ===================
+          ____||___
+\ /      /       []\
+ X=======           \__
+/ \      \____________|
+            ||  ||
+         \-----------/")
+    End Sub
+End Module
