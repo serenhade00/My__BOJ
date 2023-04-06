d4ad9b1c6df108669ed06acd78cc53c66f1fe339
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-04-06 23:59:44 +0900

    [Bronze IV] Title: A Simple Problem., Time: 48 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++)
+    {
+        long long n;
+        cin >> n;
+        cout << n*n << '\n';
+    }
+}
