8f115f2bb8dbf2fc09124187b552475689d87007
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-05-24 23:58:42 +0900

    [Bronze IV] Title: Archivist, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a == 1995) cout << "ITMO";
+    if (a == 1996) cout << "SPbSU";
+    if (a == 1997) cout << "SPbSU";
+    if (a == 1998) cout << "ITMO";
+    if (a == 1999) cout << "ITMO";
+    if (a == 2000) cout << "SPbSU";
+    if (a == 2001) cout << "ITMO";
+    if (a == 2002) cout << "ITMO";
+    if (a == 2003) cout << "ITMO";
+    if (a == 2004) cout << "ITMO";
+    if (a == 2005) cout << "ITMO";
+    if (a == 2006) cout << "PetrSU, ITMO";
+    if (a == 2007) cout << "SPbSU";
+    if (a == 2008) cout << "SPbSU";
+    if (a == 2009) cout << "ITMO";
+    if (a == 2010) cout << "ITMO";
+    if (a == 2011) cout << "ITMO";
+    if (a == 2012) cout << "ITMO";
+    if (a == 2013) cout << "SPbSU";
+    if (a == 2014) cout << "ITMO";
+    if (a == 2015) cout << "ITMO";
+    if (a == 2016) cout << "ITMO";
+    if (a == 2017) cout << "ITMO";
+    if (a == 2018) cout << "SPbSU";
+    if (a == 2019) cout << "ITMO";
+    return 0;
+}
