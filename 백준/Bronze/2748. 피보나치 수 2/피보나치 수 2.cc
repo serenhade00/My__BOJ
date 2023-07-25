104459548c4b8722bec46a440c8d17804266c3f8
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-07-25 16:45:00 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long fibo[100];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for(int i=2; i<=n; i++)
+    {
+        fibo[i] = fibo[i-1]+fibo[i-2];
+    }
+    cout << fibo[n];
+}
