7d73449b32c104123943035021c54168f00143dc
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-08-29 00:23:33 +0900

    [Bronze IV] Title: Poziome serca, Time: 144 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cout << " @@@   @@@  ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "@   @ @   @ ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "@    @    @ ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "@         @ ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << " @       @  ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "  @     @   ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "   @   @    ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "    @ @     ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "     @      ";
+    }
+    return 0;
+}
