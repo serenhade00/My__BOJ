908c5351bdb4f1d66b35d307c695c0e214c5ba90
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-10-31 00:45:50 +0900

    [Bronze III] Title: 2루수 이름이 뭐야, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17350. 2루수 이름이 뭐야/2루수 이름이 뭐야.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++)
+    {
+        string s;
+        cin >> s;
+        if(s == "anj")
+        {
+            cout << "뭐야;";
+            return 0;
+        }
+    }
+    cout << "뭐야?";
+}
