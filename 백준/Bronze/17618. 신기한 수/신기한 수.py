7c9afeb77f88bb95af2c8e987f6641c47d2c98d7
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-12-06 01:56:45 +0900

    [Bronze III] Title: 신기한 수, Time: 488 ms, Memory: 31120 KB, Score: 78 point -BaekjoonHub

---
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+ans = 0
+for n in range(1, int(input())+1):
+    s = 0
+    tmp = n
+    while tmp:
+        s += tmp%10
+        tmp //= 10
+    if n%s == 0: ans += 1
+print(ans)
