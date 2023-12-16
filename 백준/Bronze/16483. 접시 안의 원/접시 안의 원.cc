6d384b915813b1904aef5a33f7e98934ddd95c46
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2023-12-17 05:26:27 +0900

    [Bronze III] Title: 접시 안의 원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int T;
+    cin >> T;
+    float half = (float)T / 2.0;
+    cout << (int)(half * half + 0.5) << "\n";
+    return 0;
+}
