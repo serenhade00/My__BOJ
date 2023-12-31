3ff84108087184e219a07d18c10998b6cccb0f51
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-01-01 02:37:00 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int num[5];
+int main(void)
+{
+    int sum = 0;
+    for(int i=0; i<5; i++)
+    {
+        cin >> num[i];
+        sum += num[i];
+    }
+    sort(num, num+5);
+    cout << sum / 5 << '\n' << num[2];
+}
+    
