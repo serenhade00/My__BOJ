99dfd99e3c01c5f4cf8e3b3a218895d00779dff4
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-02-23 02:33:12 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a[11] = {12, 11, 11, 10, 9, 9, 9, 8, 7, 6, 6};
+    int b[11] = {1600, 894, 1327, 1311, 1004, 1178, 1357, 837, 1055, 556, 773};
+    int n; cin >> n; cout << a[n-1] << ' ' << b[n-1];
+}
