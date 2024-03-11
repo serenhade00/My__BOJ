cf2bd386e61c42aa69c4382154dc157ff04f6630
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-03-11 23:49:13 +0900

    [Bronze III] Title: Hissing Microphone, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/15272. Hissing Microphone/Hissing Microphone.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    string s;
+    cin >> s;
+    if(s.find("ss") != string::npos)
+        cout << "hiss";
+    else
+        cout << "no hiss";
+}
