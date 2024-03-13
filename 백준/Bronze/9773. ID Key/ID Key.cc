6b570d927251f011abe54218b8288fc4d78550b3
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-03-14 02:10:06 +0900

    [Bronze III] Title: ID Key, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/9773. ID Key/ID Key.cc
+++ 백준/Bronze/9773. ID Key/ID Key.cc
@@ -4,12 +4,12 @@ int main(void){
 	int t; cin >> t;
 	for(int i=0; i<t; i++){
 		string s; cin >> s;
-	    int sum = 0;
-	    for(int j=0; j<13; j++) sum += s[j] - '0';
-	    sum += stoi(s.substr(10, 3)) * 10;
-	    if(sum > 9999) cout << to_string(sum).substr(1);
-	    else if(sum < 1000) cout << sum + 1000;
-	    else cout << sum;
+	    int k = 0;
+	    for(int j=0; j<13; j++) k += s[j] - '0';
+	    k += stoi(s.substr(10, 3)) * 10;
+	    if(k > 9999) cout << to_string(k).substr(1);
+	    else if(k < 1000) cout << k + 1000;
+	    else cout << k;
 	    cout << '\n';
 	}
 }
