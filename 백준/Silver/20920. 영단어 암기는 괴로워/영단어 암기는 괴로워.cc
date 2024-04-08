6f243602418c06bc633f6dfb3e9be1fdf53beb78
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-04-09 01:03:02 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 860 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map<string,int> m;
+vector<string> v;
+
+bool cmp(const string &s1, const string &s2)
+{
+    if(m[s1] != m[s2])
+        return m[s1] > m[s2] ? true : false;
+    if(s1.size() != s2.size())
+        return s1.size() > s2.size() ? true : false;
+    return s1 < s2;
+}
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, k;
+    string s;
+    cin >> n >> k;
+    for(int i=0; i<n; i++)
+    {
+        cin >> s;
+        if(s.size() < k)
+            continue;
+        v.push_back(s);
+        m[s]++;
+    }
+    sort(v.begin(), v.end(), cmp);
+    v.erase(unique(v.begin(),v.end()), v.end());
+    for(auto e : v)
+        cout << e << '\n';
+}
