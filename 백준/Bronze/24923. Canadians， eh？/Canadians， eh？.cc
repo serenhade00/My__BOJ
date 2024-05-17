1ab32fc51b65fabe57e0d6fd932bb7fe059123d2
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-05-18 03:24:11 +0900

    [Bronze III] Title: Canadians, eh?, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24923. Canadians， eh？/Canadians， eh？.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    string s;
+    getline(cin, s);
+    int len = s.size();
+    if(s.substr(len-3) == "eh?")
+        cout << "Canadian!";
+    else
+        cout << "Imposter!";
+}
