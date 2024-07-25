e8cec01aba6438cd29d788e1ec367ac955760d0c
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-07-26 02:51:34 +0900

    [Bronze III] Title: Luhn, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14075. Luhn/Luhn.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[16];
+
+int main() {
+    string s;
+    cin >> s;
+    for (int i = 0; i < 16; ++i) {
+        arr[i] = s[i] - '0';
+    }
+    for (int i = 0; i < 16; i += 2) {
+        arr[i] = arr[i] * 2;
+    }
+    for (int i = 0; i < 16; ++i) {
+        if (arr[i] >= 10) {
+            arr[i] = arr[i] / 10 + arr[i] % 10;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < s.length(); ++i) {
+        sum += arr[i];
+    }
+    cout << (!(sum % 10) ? "DA" : "NE");
+}
