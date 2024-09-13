d24e9039d30efef5fe88eafad6ad92d9274d382f
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-09-14 02:23:05 +0900

    [Silver V] Title: 불사조, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31780. 불사조/불사조.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0); 
+    cin.tie(0);
+	ll X, M;
+	cin >> X >> M;
+	cout << X * (M + 1);
+}
