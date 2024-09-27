100bf17634a09d55f143fa642c3a96ccbebe1119
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-09-27 19:38:18 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.cc
@@ -1,2 +1,2 @@
-#include <cstdio> 
-int main(void){int n;scanf("%d", &n);(n % 7 == 2)?printf("1"):printf("0");}
+#include <cstdio>
+int main(void){int n;scanf("%d",&n);(n%7==2)?printf("1"):printf("0");}
