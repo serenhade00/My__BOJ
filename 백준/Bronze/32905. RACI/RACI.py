a7f1ca418cc358d1c934a2a5f349f1a3125671f8
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2024-12-08 05:02:45 +0900

    [Bronze IV] Title: RACI, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32905. RACI/RACI.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+flag = True
+for _ in range(n):
+    line = list(input().split())
+    if line.count("A") != 1:
+        flag = False
+print("Yes" if flag else "No")
