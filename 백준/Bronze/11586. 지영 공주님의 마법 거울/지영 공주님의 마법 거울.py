a827a16947c8ed5eee7100ccae7bd476841b5fc6
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-01-07 22:50:17 +0900

    [Bronze III] Title: 지영 공주님의 마법 거울, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/11586. 지영 공주님의 마법 거울/지영 공주님의 마법 거울.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+mirror = [input().rstrip() for _ in range(n)]
+k = int(input())
+
+if k == 1:  # 원본 출력
+    print(*mirror, sep='\n')
+elif k == 2:  # 좌우 반전
+    print(*[i[::-1] for i in mirror], sep='\n')
+else:  # 상하 반전
+    print(*mirror[::-1], sep='\n')
