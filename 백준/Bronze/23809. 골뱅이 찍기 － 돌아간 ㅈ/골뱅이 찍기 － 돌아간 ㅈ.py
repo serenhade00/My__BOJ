783ca5ee461901087cab854a0ecd462a0007fc9c
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-02-11 00:47:35 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.py
@@ -0,0 +1,11 @@
+N = int(input())
+for j in range(N):
+    print("@"*N+" "*3*N+"@"*N)
+for k in range(N):
+    print("@"*N+" "*2*N+"@"*N)
+for u in range(N):
+    print("@"*3*N)
+for o in range(N):
+    print("@"*N+" "*2*N+"@"*N) 
+for p in range(N):
+    print("@"*N+" "*3*N+"@"*N)
