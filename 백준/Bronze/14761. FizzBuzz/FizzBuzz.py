6f1d44623751e054e7e0bca259abf64803f199eb
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-05-18 00:16:39 +0900

    [Bronze III] Title: FizzBuzz, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14761. FizzBuzz/FizzBuzz.py
@@ -0,0 +1,12 @@
+X, Y, N = map(int, input().split())
+
+# 출력
+for i in range(1, N+1):
+    if i % X == 0 and i % Y == 0:
+        print("FizzBuzz")
+    elif i % X == 0:
+        print("Fizz")
+    elif i % Y == 0:
+        print("Buzz")
+    else:
+        print(i)
