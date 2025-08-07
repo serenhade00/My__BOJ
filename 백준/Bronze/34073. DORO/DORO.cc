86ba07e85f6c2b9fa322cae3a15550ccce95d2b2
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-08-07 23:12:46 +0900

    [Bronze IV] Title: DORO, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    string s;
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        cin >> s;
+        cout << s + "DORO ";
+    }
+}
