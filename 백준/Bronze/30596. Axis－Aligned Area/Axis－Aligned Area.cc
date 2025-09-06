4acc5a0b4d5db632f7d917832b8fd1c80bdd488a
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-09-07 02:03:05 +0900

    [Bronze III] Title: Axis-Aligned Area, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30596. Axis－Aligned Area/Axis－Aligned Area.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << a * c;
+}
