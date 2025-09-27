7e87a6ca3994f01083fd5c766b454c04ba0f6161
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-09-27 16:33:10 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,50 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#define MAX 1000001
 
 using namespace std;
 
-vector<int> p(1000001, -1);
+vector<int> p(MAX, -1);
 
-int get_find(int x)
+int find(int x)
 {
-    if(p[x] < 0)
-        return x;
-    return get_find(p[x]);
+	if (p[x] < 0)
+		return x;
+	return p[x] = find(p[x]);
 }
 
-int get_union(int u, int v)
+bool uni(int a, int b)
 {
-    u = get_find(u), v = get_find(v);
-    if(u == v)
-        return 0;
-    if(p[u] == p[v])
-        p[u]--;
-    if(p[u] < p[v])
-        p[v] = u;
-    else
-        p[u] = v;
-    return 1;
+	a = find(a), b = find(b);
+	if (a == b)
+		return true;
+	if (p[b] < p[a])
+		swap(a, b);
+	if (p[a] == p[b])
+		p[a]--;
+	p[b] = a;
+	return false;
 }
-
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int n, m;
-    cin >> n >> m;
-    for(int i=0; i<m; i++)
-    {
-        int val, a, b;
-        cin >> val >> a >> b;
-        if(!val)
-            get_union(a, b);
-        else
-        {
-        	a = get_find(a), b = get_find(b);
-            if(a == b)
-                cout << "YES\n";
-            else
-                cout << "NO\n";
-        }
-    }
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m, op, a, b;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> op >> a >> b;
+		if (!op)
+			uni(a, b);
+		else
+		{
+			if (find(a) == find(b))
+				cout << "YES\n";
+			else
+				cout << "NO\n";
+		}
+	}
 }
