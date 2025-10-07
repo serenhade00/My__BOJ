4cdfbf3fe90fc12e958003dec57b888fe1d3980c
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-10-08 04:48:48 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0); 
+    cin.tie(NULL);
+    ll x;
+    cin >> x;
+    cout << x / 5;
+}
