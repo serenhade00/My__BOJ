0f4caa6e925d37620a1d40d170653d93170958ff
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-12-11 23:00:13 +0900

    [Bronze I] Title: 억지부리기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/34893. 억지부리기/억지부리기.cc
+++ 백준/Bronze/34893. 억지부리기/억지부리기.cc
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-int main(void)
-{
+int main(void){
     long long a, b, c, na, nc;
     cin >> a >> b >> c;
     na = a, nc = c;
-    if(a >= c + 3)
-    {
+    if(a >= c + 3){
         na -= (a-c) / 3 * 2;
         nc += (a-c) / 3;
     }
