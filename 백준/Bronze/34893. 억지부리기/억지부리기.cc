35e397c7e7193a61dafe13615066a5a72b0ec2cc
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-12-11 22:57:55 +0900

    [Bronze I] Title: 억지부리기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34893. 억지부리기/억지부리기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    long long a, b, c, na, nc;
+    cin >> a >> b >> c;
+    na = a, nc = c;
+    if(a >= c + 3)
+    {
+        na -= (a-c) / 3 * 2;
+        nc += (a-c) / 3;
+    }
+    cout << min({na, b, nc});
+}
