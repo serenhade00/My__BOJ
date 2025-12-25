634046829c93ba6ccb817e77fc87d452d7931b90
Seonghwan Shin <101376853+serenhade00@users.noreply.github.com>
2025-12-26 00:57:28 +0900

    [Bronze II] Title: 방 배정, Time: 60 ms, Memory: 34536 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,16 @@
+import math
+
+n, k = map(int, input().split())
+arr = [[0] * 7 for _ in range(2)]  
+
+for _ in range(n):
+    s, g = map(int, input().split())
+    arr[s][g] += 1    
+    
+result = 0
+
+for i in range(2):
+    for j in range(7):
+        result += math.ceil(arr[i][j] / k)
+        
+print(result)
